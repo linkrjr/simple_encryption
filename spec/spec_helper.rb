b38b98da77fcca6a92a44435c4f86315c25891b0
Ronaldo Gomes <ronaldo.junior@gmail.com>
2013-06-25 16:40:10 +1000

    spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'simple_encryption'
+require 'rspec'
+
+Dir[File.expand_path('../support/**/*.rb', __FILE__)].each {|f| require f}
+
+RSpec.configure do |config|  
+
+end
